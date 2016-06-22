5487ffdde2c06c3b3173b02eeab3bfe3c664fe0b
Chen Edmond <issv203@iss.nus.edu.sg>
2016-06-22 11:16:39 +0800

    day 7 : update from Ken

--- day7/main.js
+++ day7/main.js
@@ -1,20 +1,33 @@
-var path = require("path");
+var path = require("path"); // import path module
 
-var express = require("express");
+var express = require("express"); // import express js framework
 var app = express();
 
-app.get("/save", function(req, res) {
-	console.log(req.query.todos);
-	var todos = JSON.parse(req.query.todos);
-	console.info(">>> todos: %s", todos);
-	console.info(">>> typeof todos: %s", typeof todos);
-	res.status(202).end();
+var bodyParser = require("body-parser"); // Body parser for fetch posted data
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); // Body parser use JSON data
+
+app.post("/save", function (req, res) { // posting todo record
+
+    console.log("Saving on server side");
+    console.log(req.body.params.todos);
+
+    var stringify = JSON.stringify(req.body.params.todos); // make sure the params from the body is json format
+    var todos = JSON.parse(stringify); // parse the json to a object
+
+    console.info(">>> todos: %s", todos); // print the values
+    console.info(">>> typeof todos: %s", typeof todos); // also print the type of the json object
+
+    res.status(202).end(); // lastly end the route path if the response status is 202
+
 });
 
 app.use("/bower_components", express.static(path.join(__dirname, "bower_components")));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("port", process.argv[2] || process.env.APP_PORT || 3000);
-app.listen(app.get("port"), function() {
-	console.info("Application started on port %d", app.get("port"));
+
+app.listen(app.get("port"), function () {
+    console.info("Application started on port %d", app.get("port"));
 });
--- day7/public/index.js
+++ day7/public/index.js
@@ -5,28 +5,29 @@ angular
 function TodoCtrl($http) {
 
     var vm = this;
-
     vm.todos = [];
-    vm.newTodo = createTodo();
+    console.log(vm.todos.length);
+    var lenTodos = vm.todos.length; // define the number of todos added
+    console.log("xxxx " + lenTodos);
+    vm.newTodo = createTodo(lenTodos); // make new instance of todo
 
     vm.addTodo = function () {
-        vm.todos.push(vm.newTodo);
-        vm.newTodo = createTodo();
-    };
-
-    vm.save = function () {
-        $http.get("/save", {
-            params: {todos: JSON.stringify(vm.todos)}
-        }).then(function () {
-            vm.todos = [];
-        }).catch(function () {
+        vm.todos.push(vm.newTodo); // push the newly added todo to the array
+        console.log(JSON.stringify(vm.newTodo));
+        console.log(vm.todos.length);
+        $http.post("/save", {  // post an ajax call to the server side
+            params: {
+                todos: JSON.stringify(vm.newTodo), // make sure the strings capture from the to do app is json format
+                headers: {'Content-Type': 'application/json'}
+            } // content type enforce
         });
+        vm.newTodo = createTodo(vm.todos.length); // reset the form fields
     };
 };
 
 
-function createTodo() {
-    return ({task: '', dueDate: '', priority: ''});
+function createTodo(lenTodos) {
+    console.log(lenTodos);
+    console.log(".....");
+    return ({id: lenTodos + 1, task: '', dueDate: '', priority: ''});
 };
-
-
